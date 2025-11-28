367399755b53d5c6375ee3d4f09409844af9466d
Paulo Cleidson <167731152+Paulo-Cleidson@users.noreply.github.com>
2025-11-28 20:05:39 -0300

    refatorando tel home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,97 +1,49 @@
-'use client';
-import { Badge } from '@/components/ui/badge';
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardHeader } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Separator } from '@/components/ui/separator'
-import { Plus, List, Check, BadgeX, Trash, ListCheck, Sigma } from 'lucide-react';
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-import EditTask from '@/components/edit-task';
-import { useState } from 'react';
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
 
-  // Captura o valor do input de cadastro
-  const [task, setTask] = useState<string>('')
-  console.log(task)
+  if (session) {
+    redirect("/task");
+  }
 
   return (
-    <main className='w-full h-screen bg-gray-100 flex justify-center items-center'>
-      <Card className='w-lg p-4'>
 
-        <CardHeader className='flex gap-2'>
-          <Input placeholder='Adicionar Tarefa' onChange={(e) => setTask(e.target.value)}/>
-          <Button> <Plus/> Cadastrar </Button>
-        </CardHeader>
+    <div 
+    className="
+    flex h-screen w-full items-center justify-center bg-linear-to-b from-orange-900
+    via-orange-700 to-orange-600 px-4
+    "
+    >
+      <div className="w-full max-w-md rounded-2xl bg-white/90 p-10 shadow-2xl backdrop-blur-md">
+        
+        <h1 className="text-4xl font-bold text-gray-900 text-center">
+          Lista de Tarefas
+        </h1>
 
-        <CardContent>
-          <Separator className='mb-4'/>
+        <div className="mt-8 flex flex-col gap-4">
+          <Link
+            href="/login"
+            className="
+            w-full rounded-full bg-orange-500 px-6 py-3 text-center font-semibold
+            text-white shadow-md transition hover:bg-orange-600"
+          >
+            Entrar
+          </Link>
 
-          <div className='flex gap-2'>
-            <Badge className='cursor-pointer' variant="default"> <List/> Todas </Badge>
-            <Badge className='cursor-pointer' variant="outline"> <BadgeX/> Não Finalizadas </Badge>
-            <Badge className='cursor-pointer' variant="outline"> <Check/> Concluidas </Badge>
-          </div>
+          <Link
+            href="/register"
+            className="w-full rounded-full border border-orange-500 px-6 py-3 text-center 
+            font-semibold text-orange-600 transition hover:bg-orange-500 hover:text-white"
+          >
+            Criar Conta
+          </Link>
+        </div>
+      </div>
+    </div>
 
-          <div className='mt-4 border-b'>
-
-            <div className='h-14 flex justify-between items-center border-t'>
-              <div className='w-1 h-full bg-green-300'></div>
-              <p className='flex-1 px-2 text-sm'>Estudar React</p>
-              <div className='flex items-center gap-3'>
-                <EditTask/>
-                <Trash size={16} className='cursor-pointer'/>
-              </div>
-            </div>
-
-          </div>
-
-          <div className='flex justify-between mt-4'>
-            <div className='flex gap-2 items-center'>
-              <ListCheck size={18}/>
-              <p className='text-sx'>Tarefas Concluídas (3/3)</p>
-            </div>
-
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button className='text-xs h-7' variant="outline">
-                  <Trash/> Limpar Tarefas Concluídas
-                </Button> 
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Tem certeza que deseja excluir x itens?</AlertDialogTitle>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogAction>Sim</AlertDialogAction>
-                  <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-
-          </div>
-
-          <div className='h-2 w-full bg-gray-100 mt-4 rounded-md'>
-            <div className='h-full bg-blue-500 rounded-md' style={{width: "50%" }}></div>
-          </div>
-
-          <div className='flex justify-end items-center gap-2'>
-            <Sigma size={18}/>
-            <p className='text-xs'> 3 tarefas no total </p>
-          </div>
-
-        </CardContent>
-
-      </Card>
-    </main>
-  )
+  );
 }
---
+++ src/app/task/page.tsx
@@ -0,0 +1,97 @@
+'use client';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Separator } from '@/components/ui/separator'
+import { Plus, List, Check, BadgeX, Trash, ListCheck, Sigma } from 'lucide-react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import EditTask from '@/components/edit-task';
+import { useState } from 'react';
+
+export default function Home() {
+
+  // Captura o valor do input de cadastro
+  const [task, setTask] = useState<string>('')
+  console.log(task)
+
+  return (
+    <main className='w-full h-screen bg-gray-100 flex justify-center items-center'>
+      <Card className='w-lg p-4'>
+
+        <CardHeader className='flex gap-2'>
+          <Input placeholder='Adicionar Tarefa' onChange={(e) => setTask(e.target.value)}/>
+          <Button> <Plus/> Cadastrar </Button>
+        </CardHeader>
+
+        <CardContent>
+          <Separator className='mb-4'/>
+
+          <div className='flex gap-2'>
+            <Badge className='cursor-pointer' variant="default"> <List/> Todas </Badge>
+            <Badge className='cursor-pointer' variant="outline"> <BadgeX/> Não Finalizadas </Badge>
+            <Badge className='cursor-pointer' variant="outline"> <Check/> Concluidas </Badge>
+          </div>
+
+          <div className='mt-4 border-b'>
+
+            <div className='h-14 flex justify-between items-center border-t'>
+              <div className='w-1 h-full bg-green-300'></div>
+              <p className='flex-1 px-2 text-sm'>Estudar React</p>
+              <div className='flex items-center gap-3'>
+                <EditTask/>
+                <Trash size={16} className='cursor-pointer'/>
+              </div>
+            </div>
+
+          </div>
+
+          <div className='flex justify-between mt-4'>
+            <div className='flex gap-2 items-center'>
+              <ListCheck size={18}/>
+              <p className='text-sx'>Tarefas Concluídas (3/3)</p>
+            </div>
+
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button className='text-xs h-7' variant="outline">
+                  <Trash/> Limpar Tarefas Concluídas
+                </Button> 
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Tem certeza que deseja excluir x itens?</AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction>Sim</AlertDialogAction>
+                  <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+
+          </div>
+
+          <div className='h-2 w-full bg-gray-100 mt-4 rounded-md'>
+            <div className='h-full bg-blue-500 rounded-md' style={{width: "50%" }}></div>
+          </div>
+
+          <div className='flex justify-end items-center gap-2'>
+            <Sigma size={18}/>
+            <p className='text-xs'> 3 tarefas no total </p>
+          </div>
+
+        </CardContent>
+
+      </Card>
+    </main>
+  )
+}
