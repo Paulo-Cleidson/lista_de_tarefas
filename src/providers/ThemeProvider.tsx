6a5402ce811fbf217cb3f5dc3c0554dcd67d03b1
Paulo Cleidson <167731152+Paulo-Cleidson@users.noreply.github.com>
2025-12-04 18:49:30 -0300

    refactoring

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import {ThemeProvider as NextThemeProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import { type ThemeProviderProps } from "next-themes";
 
 export default function ThemeProvider({
   children,
